58e0e9a31993ae4037576fa7d3d25a4836c1f250
Maria Cosmen <mcosme00@gmail.com>
2022-11-24 12:40:23 +0900

    Added styles and fixed the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ require 'rest-client'
 # TODO: Write a seed to insert 10 posts in the database fetched from the Hacker News API.
 
 serialized = RestClient.get('https://hacker-news.firebaseio.com/v0/topstories.json')
-numbers = JSON.parse(serialized).sample(10)
+numbers = JSON.parse(serialized)
 
 10.times do
   response = RestClient.get("https://hacker-news.firebaseio.com/v0/item/#{numbers.sample(1).first}.json")
